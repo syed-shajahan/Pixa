25d6b7360dc6ea1aab1f7402cc915a9f599f97eb
syed-shajahan <shajahn7shaju@gmail.com>
2024-06-11 12:53:06 +0530

    code Refactory

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,138 +1,133 @@
-import React, { useEffect, useState } from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import { IconButton, Stack } from "@mui/material";
-import Pagination from "@mui/material/Pagination";
-import CollectionsOutlinedIcon from "@mui/icons-material/CollectionsOutlined";
-import { LandingPageTitles } from "../utils/CommonConst";
-import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import PopupModal from "../components/PopupModal";
-import SearchForm from "../components/SearchForm";
-import MainCard from "../components/MainCard";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import { IconButton, Stack } from '@mui/material'
+import Pagination from '@mui/material/Pagination'
+import CollectionsOutlinedIcon from '@mui/icons-material/CollectionsOutlined'
+import { LandingPageTitles } from '../utils/CommonConst'
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import PopupModal from '../components/PopupModal'
+import SearchForm from '../components/SearchForm'
+import MainCard from '../components/MainCard'
 
 export interface IpropsData {
-  alt_description: string;
-  urls: ImageUrls;
-  sponsorship: IsponserShips;
-  user: Iusers;
+  alt_description: string
+  urls: ImageUrls
+  sponsorship: IsponserShips
+  user: Iusers
 }
 
 interface ImageUrls {
-  raw: string;
-  full: string;
-  small: string;
-  regular: string;
+  raw: string
+  full: string
+  small: string
+  regular: string
 }
 
 interface IsponserShip {
-  instagram_username: string;
-  twitter_username: string;
+  instagram_username: string
+  twitter_username: string
 }
 
 interface IsponserShips {
-  sponsor: IsponserShip;
+  sponsor: IsponserShip
 }
 
 interface IprofilePictureProps {
-  medium: string;
-  large: string;
-  regular: string;
+  medium: string
+  large: string
+  regular: string
 }
 
 interface Iusers {
-  first_name: string;
-  profile_image: IprofilePictureProps;
+  first_name: string
+  profile_image: IprofilePictureProps
 }
 
 const Landing = () => {
-  const Access_Key = "xWvqzCirtYSno64CSUFW9Qh_-RqCBbx7KgO8M6k2wiA";
-  const [data, setData] = useState<IpropsData[]>([]);
-  const [query, setQuery] = useState("");
-  const [page, setPage] = useState<number>(1);
-  const [open, setOpen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [likes, setLikes] = useState<{ [key: number]: boolean }>({});
+  const Access_Key = '4gljNh90wF9AyrmnoBbBgA8XvJJoo3LvpmjbHrKRLYY'
+  const [data, setData] = useState<IpropsData[]>([])
+  const [query, setQuery] = useState('')
+  const [page, setPage] = useState<number>(1)
+  const [open, setOpen] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [likes, setLikes] = useState<{ [key: number]: boolean }>({})
 
   useEffect(() => {
+    const API_URL = `https://api.unsplash.com/photos/?client_id=${Access_Key}&page=${page}`
     const fetchData = async () => {
-      let API_URL;
-      if (query) {
-        API_URL = `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=${page}`;
-      } else {
-        API_URL = `https://api.unsplash.com/photos/?client_id=${Access_Key}&page=${page}`;
-      }
-
       try {
-        const response = await fetch(API_URL);
-        const responseData = await response.json();
-        setData(query ? responseData.results : responseData);
+        const response = await fetch(API_URL)
+        const responseData = await response.json()
+        setData(responseData)
+        console.log(data, 'test')
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error)
       }
-    };
-    fetchData();
-  }, [page, query, Access_Key]);
+    }
+    fetchData()
+  }, [])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setPage(1);
-    const API_URL2 = `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=1`;
+    e.preventDefault()
+    const API_URL2 = `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=${page}`
 
     try {
-      const response = await fetch(API_URL2);
-      const responseData = await response.json();
-      setData(responseData.results);
+      const response = await fetch(API_URL2)
+      const responseData = await response.json()
+      setData(responseData.results)
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error)
     }
-  };
+  }
 
   const handlePageChange = (
     event: React.ChangeEvent<unknown>,
-    value: number
+    value: number,
   ) => {
-    setPage(value);
-  };
+    setPage(value)
+  }
 
   const handleClickOpen = (index: number) => {
-    setOpen(true);
-    setCurrentIndex(index);
-  };
+    setOpen(true)
+    setCurrentIndex(index)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const handlePrevImage = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? data.length - 1 : prevIndex - 1
-    );
-  };
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1,
+    )
+  }
 
   const handleNextImage = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === data.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1,
+    )
+  }
 
   const handleLike = (index: number) => {
     setLikes((prevLikes) => ({
       ...prevLikes,
       [index]: !prevLikes[index],
-    }));
-  };
+    }))
+  }
 
   return (
     <>
       <section className="landing_sec">
         <Box className="container">
           <a href="/" className="logo_txt">
-            <Typography variant="h1" textAlign={"center"} component="h1">
+            <Typography variant="h1" textAlign={'center'} component="h1">
               {LandingPageTitles.LOGO_TITLES}
               <CollectionsOutlinedIcon className="camera" />
             </Typography>
@@ -144,36 +139,37 @@ const Landing = () => {
             setQuery={setQuery}
           />
 
-          <Grid container spacing={2}>
-            {data?.map((item, index) => {
-              return (
-                <Grid item lg={4} md={6} xs={12} key={index}>
-                  <MainCard
-                    item={item}
-                    index={index}
-                    likes={likes}
-                    handleLike={handleLike}
-                    handleClickOpen={handleClickOpen}
-                  />
-                </Grid>
-              );
-            })}
-          </Grid>
-
+          <Box className="gridContainer">
+            <Grid container spacing={2}>
+              {data?.map((item, index) => {
+                return (
+                  <Grid item lg={6} md={6} xs={12} key={index}>
+                    <MainCard
+                      item={item}
+                      index={index}
+                      likes={likes}
+                      handleLike={handleLike}
+                      handleClickOpen={handleClickOpen}
+                    />
+                  </Grid>
+                )
+              })}
+            </Grid>
+          </Box>
           <Box
             sx={{
-              flex: "1",
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              marginBotton: "20px !important",
+              flex: '1',
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              marginBotton: '20px !important',
             }}
           >
             <Stack
               spacing={2}
               sx={{
-                margin: "10px auto 20px",
-                maxWidth: "800px",
+                margin: '10px auto 20px',
+                maxWidth: '800px',
               }}
             >
               <Pagination
@@ -196,7 +192,7 @@ const Landing = () => {
         />
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing

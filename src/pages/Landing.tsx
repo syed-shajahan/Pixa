9a0c3b1dd0a29942f4baa382894da00d7fa8f835
syed-shajahan <shajahn7shaju@gmail.com>
2024-06-20 11:24:43 +0530

    added react ininite scroll

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,137 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import { IconButton, Stack } from '@mui/material';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useCallback, useEffect, useState } from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import { IconButton, Stack } from '@mui/material'
 // import Pagination from '@mui/material/Pagination';
-import CollectionsOutlinedIcon from '@mui/icons-material/CollectionsOutlined';
-import { LandingPageTitles } from '../utils/CommonConst';
-import PopupModal from '../components/PopupModal';
-import SearchForm from '../components/SearchForm';
-import MainCard from '../components/MainCard';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import Preloader from '../components/Preloder';
+import CollectionsOutlinedIcon from '@mui/icons-material/CollectionsOutlined'
+import { LandingPageTitles } from '../utils/CommonConst'
+import PopupModal from '../components/PopupModal'
+import SearchForm from '../components/SearchForm'
+import MainCard from '../components/MainCard'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import Preloader from '../components/Preloder'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export interface IpropsData {
-  alt_description: string;
-  urls: ImageUrls;
-  sponsorship: IsponserShips;
-  user: Iusers;
+  alt_description: string
+  urls: ImageUrls
+  sponsorship: IsponserShips
+  user: Iusers
 }
 
 interface ImageUrls {
-  raw: string;
-  full: string;
-  small: string;
-  regular: string;
+  raw: string
+  full: string
+  small: string
+  regular: string
 }
 
 interface IsponserShip {
-  instagram_username: string;
-  twitter_username: string;
+  instagram_username: string
+  twitter_username: string
 }
 
 interface IsponserShips {
-  sponsor: IsponserShip;
+  sponsor: IsponserShip
 }
 
 interface IprofilePictureProps {
-  medium: string;
-  large: string;
-  regular: string;
+  medium: string
+  large: string
+  regular: string
 }
 
 interface Iusers {
-  first_name: string;
-  profile_image: IprofilePictureProps;
+  first_name: string
+  profile_image: IprofilePictureProps
 }
 
 const Landing = () => {
-  const Access_Key = '4gljNh90wF9AyrmnoBbBgA8XvJJoo3LvpmjbHrKRLYY';
-  const [data, setData] = useState<IpropsData[]>([]);
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState<number>(1);
-  const [open, setOpen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [likes, setLikes] = useState<{ [key: number]: boolean }>({});
-  const [loading, setLoading] = useState<boolean>(false); 
+  const Access_Key = '4gljNh90wF9AyrmnoBbBgA8XvJJoo3LvpmjbHrKRLYY'
+  const [data, setData] = useState<IpropsData[]>([])
+  const [query, setQuery] = useState('')
+  const [page, setPage] = useState<number>(1)
+  const [open, setOpen] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [likes, setLikes] = useState<{ [key: number]: boolean }>({})
+  const [loading, setLoading] = useState<boolean>(false)
 
-  useEffect(() => {
+  const fetchData = useCallback(async () => {
     const API_URL = query
       ? `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=${page}`
-      : `https://api.unsplash.com/photos/?client_id=${Access_Key}&page=${page}`;
+      : `https://api.unsplash.com/photos/?client_id=${Access_Key}&page=${page}`
 
-    const fetchData = async () => {
-      setLoading(true); // Set loading to true before fetching data
-      try {
-        const response = await fetch(API_URL);
-        const responseData = await response.json();
-        setData((prevData) => (query ? [...prevData, ...responseData.results] : [...prevData, ...responseData]));
-        console.log(responseData, 'test');
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-      setLoading(false); // Set loading to false after data is fetched
-    };
-    fetchData();
-    query && window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [page, query]);
+    setLoading(true) // Set loading to true before fetching data
+    try {
+      const response = await fetch(API_URL)
+      const responseData = await response.json()
+      setData((prevData) =>
+        query
+          ? [...prevData, ...responseData.results]
+          : [...prevData, ...responseData],
+      )
+      console.log(responseData, 'test')
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+    setLoading(false) // Set loading to false after data is fetched
+  }, [page, query])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setPage(1);
-    const API_URL2 = `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=1`;
+    e.preventDefault()
+    const API_URL2 = `https://api.unsplash.com/search/photos?query=${query}&client_id=${Access_Key}&page=1`
 
     try {
-      setLoading(true); // Set loading to true before fetching data
-      const response = await fetch(API_URL2);
-      const responseData = await response.json();
-      setData(responseData.results);
-      setLoading(false); // Set loading to false after data is fetched
+      setLoading(true) 
+      const response = await fetch(API_URL2)
+      const responseData = await response.json()
+      setData(responseData.results)
+      setLoading(false) 
     } catch (error) {
-      console.error('Error fetching data:', error);
-      setLoading(false); // Set loading to false if there is an error
+      console.error('Error fetching data:', error)
+      setLoading(false)
     }
-  };
+    setQuery(" ");
+  }
 
   const handleClickOpen = (index: number) => {
-    setOpen(true);
-    setCurrentIndex(index);
-  };
+    setOpen(true)
+    setCurrentIndex(index)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const handlePrevImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? data.length - 1 : prevIndex - 1));
-  };
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1,
+    )
+  }
 
   const handleNextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
-  };
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1,
+    )
+  }
 
   const handleLike = (index: number) => {
     setLikes((prevLikes) => ({
       ...prevLikes,
       [index]: !prevLikes[index],
-    }));
-  };
+    }))
+  }
 
   const handlePageUp = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
 
-  const handleInfiniteScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
-      setPage((prev) => prev + 1);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleInfiniteScroll);
-    return () => window.removeEventListener('scroll', handleInfiniteScroll);
-  }, []);
 
   return (
     <>
@@ -144,21 +144,43 @@ const Landing = () => {
             </Typography>
           </a>
 
-          <SearchForm handleSubmit={handleSubmit} query={query} setQuery={setQuery} />
+          <SearchForm
+            handleSubmit={handleSubmit}
+            query={query}
+            setQuery={setQuery}
+          />
 
           {loading ? (
-            <Preloader /> 
+            <Preloader />
           ) : (
             <Box className="gridContainer">
-              <Grid container spacing={2}>
-                {data?.map((item, index) => {
-                  return (
+           
+                <InfiniteScroll
+                  dataLength={data.length}
+                  next={fetchData}
+                  hasMore={true}
+                  loader={<h4>Loading...</h4>}
+                  endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                      <b>Yay! You have seen it all</b>
+                    </p>
+                  }
+                >
+                     <Grid container spacing={2}>
+                  {data?.map((item, index) => (
                     <Grid item lg={6} md={6} xs={12} key={index}>
-                      <MainCard item={item} index={index} likes={likes} handleLike={handleLike} handleClickOpen={handleClickOpen} />
+                      <MainCard
+                        item={item}
+                        index={index}
+                        likes={likes}
+                        handleLike={handleLike}
+                        handleClickOpen={handleClickOpen}
+                      />
                     </Grid>
-                  );
-                })}
-              </Grid>
+                  ))}
+                    </Grid>
+                </InfiniteScroll>
+            
             </Box>
           )}
 
@@ -202,7 +224,7 @@ const Landing = () => {
         </IconButton>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
